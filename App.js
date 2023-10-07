83bd0eaace33201562e387f44a0a491f7b16d371
Grantee <144291514+granteecodes@users.noreply.github.com>
2023-10-06 19:18:40 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Counter  from './counter';
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>Simple React Counter</h1>
+      <Counter />
+    </div>
+  )
+}
+
+export default App;
